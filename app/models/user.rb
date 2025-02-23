c4ff44c902cce259cae1612a7edbdbd9ff9555f6
yukten <yuk7829@gmail.com>
2025-02-23 18:29:08 +0900

    「いいね」をできるように＆「いいね」がついているか確認できるように

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,6 +32,10 @@ class User < ApplicationRecord
     articles.exists?(id: article.id)
   end
 
+  def has_liked?(article)
+    likes.exists?(article_id: article.id)
+  end
+
   def display_name
     # ぼっち演算子
     profile&.nickname || self.email.split('@').first
